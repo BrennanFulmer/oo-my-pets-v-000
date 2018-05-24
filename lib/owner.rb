affafb8ed358b0dd68f7b2630b5f4e6de7fd4373
Brennan Fulmer <brennan.flmr@gmail.com>
2018-05-24 20:03:06 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -66,17 +66,7 @@ class Owner
     self.pets.clear
   end
   
-  
   def list_pets
     "I have #{self.pets[:fishes].size} fish, #{self.pets[:dogs].size} dog(s), and #{self.pets[:cats].size} cat(s)."
   end
-  
-  
-  
 end
-
-
-
-
-
-# spacing
